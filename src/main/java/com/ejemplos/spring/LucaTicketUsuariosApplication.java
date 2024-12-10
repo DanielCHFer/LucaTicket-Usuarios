2a2784c06bd6567bb53703e4ede7676b8bc9d597
Dani <danielchfer@gmail.com>
2024-12-10 16:26:29 +0100

    Commit Inicial Dev

--- src/main/java/com/ejemplos/spring/LucaTicketUsuariosApplication.java
+++ src/main/java/com/ejemplos/spring/LucaTicketUsuariosApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LucaTicketUsuariosApplication {
-
+	// socorro
 	public static void main(String[] args) {
 		SpringApplication.run(LucaTicketUsuariosApplication.class, args);
 	}
