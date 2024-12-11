5eb589538df045518d627c23ccaec353bc195b17
Dani <danielchfer@gmail.com>
2024-12-11 09:33:44 +0100

    Metodo Post en Controller

---
+++ src/main/java/com/ejemplos/spring/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package com.ejemplos.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ejemplos.spring.adapter.UsuariosAdapter;
+import com.ejemplos.spring.model.Usuario;
+import com.ejemplos.spring.response.UsuarioResponse;
+import com.ejemplos.spring.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@Autowired
+	UsuariosAdapter usuarioAdapter;
+	
+	@PostMapping
+    public ResponseEntity<Usuario> crearUsuario(@Valid @RequestBody UsuarioResponse usuarioResponse) {
+		Usuario usuario = usuarioService.saveUsuario(usuarioAdapter.of(usuarioResponse)).get();
+        return ResponseEntity.ok(usuario);
+    }
+}
