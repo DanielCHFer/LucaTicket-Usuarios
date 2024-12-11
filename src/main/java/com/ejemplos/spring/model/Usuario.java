fe06caa987df22d59f88b870d5d38c242023f7b3
nosaaaaya <iyahen14@gmail.com>
2024-12-11 08:54:29 +0100

    cambios

---
+++ src/main/java/com/ejemplos/spring/model/Usuario.java
@@ -0,0 +1,74 @@
+package com.ejemplos.spring.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_usuario;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private Date fecha_alta;
+	
+	public Usuario( ) {
+		super();
+	}
+	
+	public Usuario( String nombre, String apellido, String email, Date fecha_alta) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.fecha_alta = fecha_alta;
+	}
+	
+	public Usuario(Long id_usuario, String nombre, String apellido, String email, Date fecha_alta) {
+		super();
+		this.id_usuario = id_usuario;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.fecha_alta = fecha_alta;
+	}
+	
+	public Long getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(Long id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getFecha_alta() {
+		return fecha_alta;
+	}
+	public void setFecha_alta(Date fecha_alta) {
+		this.fecha_alta = fecha_alta;
+	}
+}
