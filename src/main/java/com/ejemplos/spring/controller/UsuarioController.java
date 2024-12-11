706ed217ee97d283977031b11063832849eb1992
Dani <danielchfer@gmail.com>
2024-12-11 12:19:36 +0100

    ResponseEntity adaptaday OpenApi

--- src/main/java/com/ejemplos/spring/controller/UsuarioController.java
+++ src/main/java/com/ejemplos/spring/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.ejemplos.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.ejemplos.spring.model.Usuario;
 import com.ejemplos.spring.response.UsuarioResponse;
 import com.ejemplos.spring.service.UsuarioService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @RestController
@@ -24,14 +26,24 @@ public class UsuarioController {
 	@Autowired
 	UsuariosAdapter usuarioAdapter;
 	
+	@Operation(
+		summary = "Dar de alta un nuevo usuario",
+		description = "Permite crear un nuevo usuario en la base de datos. Ignora el Id_usuario si se especifica en el Json de entrada."
+	)
 	@PostMapping
-    public ResponseEntity<Usuario> crearUsuario(@Valid @RequestBody UsuarioResponse usuarioResponse) {
+    public ResponseEntity<UsuarioResponse> crearUsuario(@Valid @RequestBody UsuarioResponse usuarioResponse) {
+			
+			//Se ignora el id si se envía
 			if (usuarioResponse.getId_usuario() != null) {
 				usuarioResponse.setId_usuario(null);
 			}
 			
-			Usuario usuario = usuarioService.saveUsuario(usuarioAdapter.of(usuarioResponse)).get();
-	        return ResponseEntity.ok(usuario);
-
+			// Se guarda el Usuario covertido a Entidad
+			UsuarioResponse res = usuarioAdapter.of(usuarioService.saveUsuario(usuarioAdapter.of(usuarioResponse)).get());
+			
+			//Se construye la ResponseEntity con el código 201
+	        return ResponseEntity
+	        		.status(HttpStatus.CREATED)
+	        		.body(res);
     }
 }
